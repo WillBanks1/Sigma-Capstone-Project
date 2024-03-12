12626da4748bcd864fdb5bb0fe0842f80489643e
WillBanks1 <wbanks@Wills-MacBook-Pro.local>
2024-03-12 13:22:23 +0000

    adding player_greeting

---
+++ player_greeting.py
@@ -0,0 +1,5 @@
+def player_greeting(player):
+    print(f'Hi player {player}')
+    print(f'Your hand is: {players_cards[player-1]}')
+    print(f'Top card of faced up pile is: {faced_up_pile[0]}')
+    return
