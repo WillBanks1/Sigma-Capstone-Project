0935fce4ffffd76a0e3cea78ca7393f54bf60e48
WillBanks1 <wbanks@Wills-MacBook-Pro.local>
2024-03-12 13:25:32 +0000

    adding check set

---
+++ check_set.py
@@ -0,0 +1,7 @@
+def check_set(played_cards):
+    set_check = [card[1:] for card in played_cards]
+    if (len(set(set_check)) != 1) or (len(played_cards)) == 1:
+        print('Invalid Play, Try Again')
+        played_cards.clear()
+
+    return played_cards
