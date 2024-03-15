a80cce15111cd97a52fea0aa7cc370176759325e
WillBanks1 <64639470+WillBanks1@users.noreply.github.com>
2024-03-15 14:00:54 +0000

    now describes faced up pile based on previous player card play

--- player_greeting.py
+++ player_greeting.py
@@ -1,5 +1,10 @@
-def player_greeting(player):
+def player_greeting(player,card_count):
     print(f'Hi player {player}')
     print(f'Your hand is: {players_cards[player-1]}')
-    print(f'Top card of faced up pile is: {faced_up_pile[0]}')
-    return
+    print(f'The previous player played {card_count} card(s)')
+    if (card_count == 1) or (card_count == 0):
+        print(f'Top card of faced up pile is: {faced_up_pile[0]}')
+    else:
+        print(f'The top {card_count} cards of the faced up pile are: {faced_up_pile[:card_count]}')
+    
+    return 
